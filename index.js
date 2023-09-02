376ffe9b87d794e9568b90780ee3006939c3bd44
IsrakUAP <israk2301@gmail.com>
2023-09-02 10:39:14 +0600

    responsive added for multiple devices

--- index.js
+++ index.js
@@ -53,14 +53,19 @@ const categoryLoad = async (categoryId) => {
     const dataCheck = data.data.find(item => item.category_id === checkCategory);
     if (categoryId === checkCategory && !dataCheck) {
         const emptyTab = document.createElement("div");
-        emptyTab.innerHTML = `<div class="flex justify-center items-center h-[700px] ml-[700px] w-[500px]"><div></div>
-        <div class="text-center"><img class="ml-16 mb-4" src="./Icon.png" alt="">
-        <p class="font-bold text-[25px]">Oops!! Sorry, There is no<br> content here</p></div></div>`
+        emptyTab.innerHTML = `<div class="flex justify-center items-center h-[700px] mx-auto ml-0 w-full md:ml-[200px] lg:ml-[700px] lg:w-[500px]">
+        <div></div>
+        <div class="text-center">
+            <img class="ml-16 mb-4" src="./Icon.png" alt="">
+            <p class="font-bold text-[25px]">Oops!! Sorry, There is no<br> content here</p>
+        </div>
+    </div>
+    `
         cardContainer.appendChild(emptyTab);
     }
     console.log(data.data);
 };
-document.getElementById("sort-by-view").addEventListener("click", function(){
+document.getElementById("sort-by-view").addEventListener("click", function () {
 
     const categoryLd = async (categoryId) => {
         const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
@@ -70,9 +75,9 @@ document.getElementById("sort-by-view").addEventListener("click", function(){
         data.data.forEach(video => {
             video.others.views = parseInt(video.others.views);
         });
-    
+
         data.data.sort((a, b) => b.others.views - a.others.views);
-    
+
         data.data.forEach(video => {
             const cardList = document.createElement("div");
             cardList.className = "card";
@@ -99,29 +104,33 @@ document.getElementById("sort-by-view").addEventListener("click", function(){
                     </div>
                     </div>
                 `;
-    
-    
+
+
             cardContainer.appendChild(cardList);
             if (postDate === "") {
                 const timeTag = cardList.querySelector(".time-tag");
                 timeTag.style.display = "none";
             }
-    
+
         });
         const checkCategory = "1005";
         const dataCheck = data.data.find(item => item.category_id === checkCategory);
         if (categoryId === checkCategory && !dataCheck) {
             const emptyTab = document.createElement("div");
-            emptyTab.innerHTML = `<div class="flex justify-center items-center h-[700px] ml-[700px] w-[500px]"><div></div>
-            <div class="text-center"><img class="ml-16 mb-4" src="./Icon.png" alt="">
-            <p class="font-bold text-[25px]">Oops!! Sorry, There is no<br> content here</p></div></div>`
+            emptyTab.innerHTML = `<div class="flex justify-center items-center h-[700px] mx-auto ml-0 w-full md:ml-[200px] lg:ml-[700px] lg:w-[500px]">
+            <div></div>
+            <div class="text-center">
+                <img class="ml-16 mb-4" src="./Icon.png" alt="">
+                <p class="font-bold text-[25px]">Oops!! Sorry, There is no<br> content here</p>
+            </div>
+        </div>`
             cardContainer.appendChild(emptyTab);
         }
         console.log(data.data);
     };
     categoryLd("1000");
     categoryLd("1001");
-    categoryLd("1003"); 
+    categoryLd("1003");
 });
 
 
